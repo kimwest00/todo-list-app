ff8b891cf844a5330cb2ed939086722f71001132
ggsong0328 <ggsong0328@gmail.com>
2024-12-02 19:21:08 +0900

    fix/강규 에러 수정

--- app/src/main/java/com/mobile/programming/todo/MainActivity.kt
+++ app/src/main/java/com/mobile/programming/todo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mobile.programming.todo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -18,9 +19,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Toolbar를 ActionBar로 설정
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        // NavigationUI와 연동
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 

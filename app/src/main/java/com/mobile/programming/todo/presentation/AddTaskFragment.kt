b0d7992b6d70b08c7ac65e55c382b0600d9aa915
kimwest00 <kimwest00@hufs.ac.kr>
2024-12-08 22:29:42 +0900

    feat: add image screen

--- app/src/main/java/com/mobile/programming/todo/presentation/AddTaskFragment.kt
+++ app/src/main/java/com/mobile/programming/todo/presentation/AddTaskFragment.kt
@@ -1,15 +1,20 @@
 package com.mobile.programming.todo.presentation
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.mobile.programming.todo.R
 import com.mobile.programming.todo.data.Task
 import com.mobile.programming.todo.databinding.FragmentAddTaskBinding
+import com.mobile.programming.todo.util.convertUriToBitmap
 
 class AddTaskFragment : Fragment() {
 
@@ -27,7 +32,10 @@ class AddTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.saveButton.setOnClickListener {
+        binding.ivArtist.setOnClickListener {
+            getContent.launch("image/*")
+        }
+        binding.btnSave.setOnClickListener {
             val task = Task(
                 title = binding.editTextTaskTitle.text.toString(),
                 description = binding.editTextTaskDescription.text.toString()
@@ -36,4 +44,20 @@ class AddTaskFragment : Fragment() {
             findNavController().navigate(R.id.action_addTaskFragment_to_taskListFragment)
         }
     }
+
+    private var insertValue = emptyList<Long>()
+    private var bitmap: Bitmap? = null
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                Log.d("ADD", "uri: $uri")
+                binding.ivArtist.setImageURI(uri)
+                context?.let { it1 ->
+                    var tmpBitmap = convertUriToBitmap(uri, it1)
+                    tmpBitmap?.let { it2 -> bitmap = it2 }
+                }
+
+            }
+        }
 }
